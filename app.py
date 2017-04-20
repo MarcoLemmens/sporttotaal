b960b6af1cd2fe4d27dbb6db7ea7cba6d07e899b
MarcoLemmens <marcolemmens@me.com>
2017-04-20 16:28:17 +0200

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def webhook():
 
 
 def processRequest(req):
-    if req.get("result").get("action") != "yahooWeatherForecast":
+    if req.get("result").get("action") == "yahooWeatherForecast":
         
     baseurl = "https://query.yahooapis.com/v1/public/yql?"
     yql_query = makeYqlQuery(req)
@@ -48,7 +48,7 @@ def processRequest(req):
     res = makeWebhookResult(data)
     return res
 
-    else if req.get("result").get("action") != "context-player":
+    else if req.get("result").get("action") == "context-player":
 return {
         "speech": "That would be Eden Hazard",
         "displayText": "Eden Hazard",
